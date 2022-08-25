4208d43629be5ea56f0b4e5aa9462def051f56e6
GusRond <gustavorondello99@outlook.com>
2022-08-25 19:30:40 -0300

    feat: PostCategory Model criado

---
+++ src/database/migrations/20220825221044-create-post-category.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
---
+++ src/database/models/postCategory.js
@@ -0,0 +1,25 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define("PostCategory", {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, { 
+      as: 'categories',
+      through: PostCategory, 
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
